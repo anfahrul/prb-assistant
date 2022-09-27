66ebea591d55dd7e7ff9025da67ff46b8a9281fa
anfahrul <an.fahrulan@gmail.com>
2022-09-28 01:15:18 +0800

    make layout of  medicine receipe

---
+++ app/src/main/java/com/example/prbassistant/Adapter/ListMedicineAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.prbassistant.Adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prbassistant.Medicine
+import com.example.prbassistant.R
+
+class ListMedicineAdapter(private val listMedicine: ArrayList<Medicine>): RecyclerView.Adapter<ListMedicineAdapter.ListViewHolder>() {
+    inner class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    var tvName: TextView = itemView.findViewById(R.id.text_receipe_name)
+    var tvAmount: TextView = itemView.findViewById(R.id.text_receipe_amount)
+    var tvPortion: TextView = itemView.findViewById(R.id.text_receipe_portion)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_medicine, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val medicine = listMedicine[position]
+        holder.tvName.text = medicine.name
+        holder.tvAmount.text = medicine.amount.toString()
+        holder.tvPortion.text = medicine.portion.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return listMedicine.size
+    }
+}
--- app/src/main/java/com/example/prbassistant/PharmacyFragment.kt
+++ app/src/main/java/com/example/prbassistant/PharmacyFragment.kt
@@ -5,55 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.prbassistant.Adapter.ListMedicineAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [PharmacyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class PharmacyFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var rvMedicine: RecyclerView
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<ListMedicineAdapter.ListViewHolder>? = null
+    private var list: ArrayList<Medicine> = arrayListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        list.addAll(MedicinesData.listData)
         return inflater.inflate(R.layout.fragment_pharmacy, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PharmacyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PharmacyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rvMedicine = view.findViewById(R.id.rv_heroes)
+        rvMedicine.setHasFixedSize(true)
+
+        rvMedicine.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = ListMedicineAdapter(list)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        list.clear()
     }
 }
