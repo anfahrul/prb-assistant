972a4da9d5c2e96e1775fd7a6f2a427a0627aaa0
anfahrul <an.fahrulan@gmail.com>
2022-09-28 12:09:06 +0800

    create selected feature of pharmacy

--- app/src/main/java/com/example/prbassistant/adapter/ListPharmacyAdapter.kt
+++ app/src/main/java/com/example/prbassistant/adapter/ListPharmacyAdapter.kt
@@ -1,8 +1,12 @@
 package com.example.prbassistant.adapter
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.prbassistant.model.Medicine
@@ -10,9 +14,12 @@ import com.example.prbassistant.R
 import com.example.prbassistant.model.Pharmacy
 
 class ListPharmacyAdapter(private val listPharmacy: ArrayList<Pharmacy>): RecyclerView.Adapter<ListPharmacyAdapter.ListViewHolder>() {
+    private var selectedItemPosition: Int = -1
+
     inner class ListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-    var tvName: TextView = itemView.findViewById(R.id.text_pharmacy_name)
-    var tvAaddress: TextView = itemView.findViewById(R.id.text_pharmacy_address)
+        var tvName: TextView = itemView.findViewById(R.id.text_pharmacy_name)
+        var tvAaddress: TextView = itemView.findViewById(R.id.text_pharmacy_address)
+        val cardView: RelativeLayout = itemView.findViewById(R.id.layout_selected_item)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -24,6 +31,17 @@ class ListPharmacyAdapter(private val listPharmacy: ArrayList<Pharmacy>): Recycl
         val pharmacy = listPharmacy[position]
         holder.tvName.text = pharmacy.name
         holder.tvAaddress.text = pharmacy.address
+
+        holder.itemView.setOnClickListener {
+            selectedItemPosition = position
+            notifyDataSetChanged()
+        }
+
+        if(selectedItemPosition == position) {
+            holder.cardView.setBackgroundColor(Color.parseColor("#eeeeee"))
+        } else {
+            holder.cardView.setBackgroundColor(Color.parseColor("#FFFFFF"))
+        }
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/prbassistant/model/PharmacyData.kt
+++ app/src/main/java/com/example/prbassistant/model/PharmacyData.kt
@@ -2,18 +2,20 @@ package com.example.prbassistant.model
 
 object PharmacyData {
     private val pharmacyName = arrayOf(
+        "Apotek Kimia Farma Wiyung",
         "Apotik Optima",
-        "Apotik 2 Oscar",
         "Apotik KIMIA FARMA 304",
-        "Apotik KIMIA FARMA 25",
-        "Apotek Kimia Farma Wiyung")
+        "Apotik 2 Oscar",
+        "Apotik KIMIA FARMA 25"
+    )
 
     private val pharmacyAddress = arrayOf(
+        "Jl. Raya Menganti A-17",
         "Jl. Raya Rungkut Mapan Fa-7",
         "Jl. Dharmawangsa No.28 ",
-        "Perak Timur 166",
         "Jl Raya Darmo No. 2-4 Surabaya",
-        "Jl. Raya Menganti A-17 | 031-7668267")
+        "Perak Timur 166"
+    )
 
     val listData: ArrayList<Pharmacy>
         get() {
