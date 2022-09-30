3b5d7c9d51418fbf1520c2f28a0d8d83f6c013af
anfahrul <an.fahrulan@gmail.com>
2022-09-30 11:10:50 +0800

    customize medicine reminder layout

--- app/src/main/java/com/example/prbassistant/ui/pharmacyreminder/MedicineReminderFragment.kt
+++ app/src/main/java/com/example/prbassistant/ui/pharmacyreminder/MedicineReminderFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
 import com.example.prbassistant.R
+import com.example.prbassistant.ui.bookcontrol.ControlBookFragmentDirections
 
-class MedicineReminderFragment : Fragment() {
+class MedicineReminderFragment : Fragment(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.show()
@@ -20,5 +23,22 @@ class MedicineReminderFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_medicine_reminder, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var btnAddNewReminder: Button = view.findViewById(R.id.btn_add_new_reminder)
+
+        btnAddNewReminder.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id) {
+            R.id.btn_add_new_reminder -> {
+                val action = MedicineReminderFragmentDirections.actionMedicineReminderFragment3ToAddMedicineFragment()
+                findNavController().navigate(action)
+            }
+        }
     }
 }
